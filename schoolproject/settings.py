26d0e08335e12b6b131170b3ead122cd42add3c7
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-27 14:54:19 +0300

    added .env file

--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -64,7 +64,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$1ahlv!$j&a41g=^!%bydst(@q_!)l%s!42-9(@ljdjk*-3h2r'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
