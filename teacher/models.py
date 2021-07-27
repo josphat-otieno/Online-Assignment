033199423d2e6d021d9f80cba9cb40173f1389ee
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-27 14:34:29 +0300

    define teacher and student model

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Teacher(models.Model):
+    teacher_name = models.CharField(max_length=120)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher')
+
+class Quiz(models.Model):
+    pass
+
+class Question(models.Model):
+    pass
+
