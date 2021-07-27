cf3775d057d36c2de003526797b3567d51ea78d2
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-28 00:39:35 +0300

    created model quiz inside the quiz app

---
+++ quizes/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+DIFF_CHOICES =(
+    ('easy,', 'easy'),
+    ('medium', 'medium'),
+    ('hard', 'hard'),
+)
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField
+    time = models.IntegerField(help_text="durations of the quiz in minutes")
+    requred_score_to_pass = models.IntegerField(help_text="score in %")
+    difficulty = models.CharField(max_length=10, choices=DIFF_CHOICES)
+
+    def __str__(self):
+        return f"{self.name}--{self.topic}"
+
+    def get_question(self):
+        pass
+
